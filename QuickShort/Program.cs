637bc7ac19aabdd72c4015f56190efa1486d051a
SalmanLS <salmanluthfi456@gmail.com>
2022-10-17 16:51:23 +0700

    create array and int variable

--- QuickShort/Program.cs
+++ QuickShort/Program.cs
@@ -4,7 +4,10 @@ namespace QuickSoort
 {
     class Program
     {
+        //array of integers to hold values
         private int[] arr = new int[20];
+        private int cmp_count = 0;
+        private int mov_count = 0;
 
     }
 }
