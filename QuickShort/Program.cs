e13181931cd7c38ab967ab53baceea9028e90d01
SalmanLS <salmanluthfi456@gmail.com>
2022-10-17 17:59:49 +0700

    create method to run the program, create object, and calling all the method

--- QuickShort/Program.cs
+++ QuickShort/Program.cs
@@ -122,5 +122,21 @@ namespace QuickSort
         {
             return n;
         }
+
+        static void Main(string[] args)
+        {
+            //Declaring the object of the class
+            Program mylist = new Program();
+            //Accept Array Elements
+            mylist.read();
+            //Calling the sorting function
+            //First call to quick sort Alogarithm
+            mylist.q_sort(0, mylist.getSize() - 1);
+            //Display sorted array
+            mylist.display();
+            //to exit from the console
+            Console.WriteLine("\n\n Press enter to exit.");
+            Console.Read();
+        }
     }
 }
